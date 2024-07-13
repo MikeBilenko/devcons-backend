3305dea219c4f56baf079c816a2f2d1dae2f374c
mike <mike@cdontiy.com>
2024-07-14 00:15:19 +0300

    new changes to dev and prod:

---
+++ local.Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.12
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install --upgrade pip
+RUN apt-get update && apt-get install -y python3-pip python3-cffi
+RUN pip install -r requirements.txt
+COPY . /app
+COPY entrypoint.sh /entrypoint.sh
+COPY start.sh /start.sh
+RUN chmod +x /entrypoint.sh
+ENV PYTHONUNBUFFERED 1
+ENTRYPOINT ["/entrypoint.sh"]
--- src/settings/dev.py
+++ src/settings/dev.py
@@ -1,4 +1,5 @@
 from .base import *
+from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -7,16 +8,13 @@ DEBUG = True
 SECRET_KEY = "django-insecure-z_=#3tsg1fgnm!a5h_ct2p8__v*0f(arl6ul#mjh!a(%$&p*bv"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost",
-    "*",
-    "http://192.168.0.204:3000"
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default=' ').split()
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default=' ').split()
+
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 
 try:
     from .local import *
@@ -26,10 +24,11 @@ except ImportError:
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'mbdevcons@gmail.com'
-EMAIL_HOST_PASSWORD = 'cgnx yxsc dstn hkyn'
-SITE_NAME = "MbDev Cons"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_FROM = config("EMAIL_FROM")
+SITE_NAME = config("SITE_NAME")
