6c3807b4882b64b17fb204a423b8d0ac9c2705c3
mike <mike@cdontiy.com>
2024-07-13 13:20:28 +0300

    proj

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,6 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-gunicorn high_hill.wsgi:application --bind 0.0.0.0:8000
+gunicorn src.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
